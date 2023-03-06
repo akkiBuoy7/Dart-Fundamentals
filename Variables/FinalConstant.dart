461aea998b03637aea909ef56c7271c3458f46d2
akkiB <akash3350saha@gmail.com>
2023-03-06 20:18:37 +0530

    Final and const variables

---
+++ Variables/FinalConstant.dart
@@ -0,0 +1,28 @@
+/*
+Final and Const variable is immutable this it's value can't be changed
+
+# Final variable is lazy i.e it is initialised only when it is accessed
+Whereas Const variable needs to be initialised during compilation only.
+ */
+
+/*
+Instance variable can not be a const variable i.e inside a class we can't
+declare a variable as const.
+Here we need to use static const
+ */
+
+
+class SomeClass{
+
+
+   final stateName = "Karnatak";
+   // stateName = "Delhi";  Not possible to reassign values
+
+   //const PI=3.14; // can not declare const variable inside a class
+   static const PI = 3.14; // need to use static keyword
+}
+
+main(){
+   print(SomeClass.PI); // since static so can be accessed by class definition
+   print(SomeClass().stateName);
+}
