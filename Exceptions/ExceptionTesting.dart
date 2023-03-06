fa828e24a4bfc56608966c80cb37de894e7df293
akkiB <akash3350saha@gmail.com>
2023-03-07 00:49:33 +0530

    Exception

---
+++ Exceptions/ExceptionTesting.dart
@@ -0,0 +1,73 @@
+void main(){
+
+  // This will give exception
+
+  // int result = 10~/0;
+  // print("result is $result");
+
+
+  // case1: When we know what kind of exception might occur, we should use on
+  print("\ncase 1");
+  try{
+    int result = 10~/0;
+    print("result is $result");
+  } on UnsupportedError{
+    print("Can not be divided by zero");
+  }
+
+  /* case2: When we don't know what kind of exception might occur, we should
+  use catch
+  */
+  print("\ncase 2");
+  try{
+    int result = 10~/0;
+    print("result is $result");
+  } catch (e){
+    print("Caught Exception is $e");
+  }
+
+  /* case3: When we want to print the exception as well as the stack trace
+  */
+  print("\ncase 3");
+  try{
+    int result = 10~/0;
+    print("result is $result");
+  } catch (e,s){
+    print("STACK TRACE");
+    print("Caught Exception is $e");
+    print("Stack trace is $s");
+  }
+
+  /* case4: When we want to print some code whether exception occur or not
+  */
+  print("\ncase 4");
+  try{
+    int result = 10~/0;
+    print("result is $result");
+  } catch (e){
+    print("STACK TRACE");
+    print("Caught Exception is $e");
+  }finally{
+    print("Finally this code will be executed");
+  }
+
+  /* case5: When we want throw a custom Exception
+  */
+  print("\ncase 5");
+  try{
+    depositAmount(-1000);
+  } on CustomException{
+    print("Caught Exception is ${CustomException().customErrorMessage()}");
+  }
+
+}
+
+class CustomException{
+  String customErrorMessage() => "Deposit amount can not be less than 0";
+}
+
+void depositAmount(int value){
+  if(value<0){
+    throw CustomException();
+  }
+}
