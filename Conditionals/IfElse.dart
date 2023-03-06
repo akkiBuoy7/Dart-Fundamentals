ba9bf8235988f0891a7aa30b31e4e6beefda6341
akkiB <akash3350saha@gmail.com>
2023-03-06 20:38:22 +0530

    Conditional Expression

--- Conditionals/IfElse.dart
+++ Conditionals/IfElse.dart
@@ -6,6 +6,7 @@ void main(){
 
   var salary = 900000;
 
+  // if else
   if(salary>=750000){
     print("Taxable under new regime");
   }else if(salary>500000 && salary<750000){
@@ -13,4 +14,26 @@ void main(){
   }else{
     print("Not Taxable");
   }
+
+  // Ternary operator
+  var a= 9 ;
+  var b = 10;
+
+  // traditional way
+  // if(a>b){
+  //   print("a is greater");
+  // }else{
+  //   print("b is greater");
+  // }
+
+  var greaterNum = a>b ? "a is greater" : "b is greater";
+  print(greaterNum);
+
+  // Conditional Expression
+  /*
+  If value is null will return the statement 2 after :
+   */
+  var name = null;
+  var result  = name ?? "Guest User";
+  print(result);
 }
