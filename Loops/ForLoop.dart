7b12d01054e640626efb2d1d7afe615b54f4fde7
akkiB <akash3350saha@gmail.com>
2023-03-06 21:04:45 +0530

    For loops

---
+++ Loops/ForLoop.dart
@@ -0,0 +1,13 @@
+main(){
+
+  // Like JAVA
+  for(var i=0;i<4;i++){
+    print(i);
+  }
+
+  // Like Kotlin
+  List planets = ["Mars","Venus","Jupiter"];
+  for(var p in planets){
+    print(p);
+  }
+}
