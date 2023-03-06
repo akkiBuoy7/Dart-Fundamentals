076aa500104069b7f314c2475cd6e1969e611f5c
akkiB <akash3350saha@gmail.com>
2023-03-06 20:46:46 +0530

    Switch case

---
+++ Conditionals/Switch.dart
@@ -0,0 +1,19 @@
+
+/*
+Very similar to JAVA
+ */
+void main(){
+  var state = "Bengaluru";
+  // only String and int is allowed in switch argument
+  switch(state){
+    case "Bengaluru":
+      print("Karnataka");
+      break;
+    case "Kolkata":
+      print("West Bengal");
+      break;
+    case "Agartala":
+      print("Tripura");
+      break;
+  }
+}
